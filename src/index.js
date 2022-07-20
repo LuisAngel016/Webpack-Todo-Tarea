d0c48f6bc6ebcf44b622d28bcdc05ca66f054451
Luis Angel <luisangelbustamante2003@gmail.com>
2022-07-19 19:06:20 -0500

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import { saludar } from './js/componentes.js';
-import './styles.css';
+import './css/styles.css';
+
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
+
+export const todoList = new TodoList();
+
+const newTodo = new Todo('Aprender javaScrip');
+// todoList.nuevoTodo( newTodo );
 
 
+// console.log( todoList );
+// crearTodoHtml( tarea );
 
-const nombre = 'Luis Angel';
+// localStorage.setItem('mi-key', 'LUIS0016');
+// sessionStorage.setItem('mi-key', 'LUIS0016');
 
-saludar( nombre );
+/* setTimeout(() => {
+    
+    localStorage.removeItem('mi-key');
+}, 1500); */
+
+// newTodo.imprimirClase();
+
+todoList.todos.forEach( crearTodoHtml );
+
+// todoList.todos[0].imprimirClase();
+
+// console.log('todos', todoList.todos);
